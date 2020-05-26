48b565228b8d4578655be9801ae004e9b73663b8
Diego Mossmann <diegomossmann@MB-003758.local>
2020-05-26 00:06:45 -0300

    caminho

--- src/backend/src/routes.js
+++ src/backend/src/routes.js
@@ -15,7 +15,7 @@ const routes = new Router();
 const upload = multer(multerConfig);
 
 const configs = {
-  caminho: "../../../build", //Aqui será definido a pasta de saída onde contém o index.html e os outros arquivos.
+  caminho: "../../build", //Aqui será definido a pasta de saída onde contém o index.html e os outros arquivos.
   forcarHTTPS: false, //Defina para true se desejar que o redirecionamento para HTTPS seja forçado (é necessário certificado SSL ativo)
   port: process.env.PORT || 3000
 }
