7e5137c226767b056f370ff398ab28442a0478c7
Diego Mossmann <diegomossmann@MB-003758.local>
2020-05-29 13:26:40 -0300

    change front

--- api/src/config/auth.js
+++
@@ -1,4 +0,0 @@
-export default {
-  secret: 'b7256732b5fd3bf62bf5a110edb385d4',
-  expiresIn: '7d',
-};
--- api/src/server.js
+++
@@ -1,7 +0,0 @@
-import app from './app.js';
-
-// app.listen(3333);
-
-app.listen(3333, function() {
-  console.log('Ok, server rodando na porta 3333!')
-});
