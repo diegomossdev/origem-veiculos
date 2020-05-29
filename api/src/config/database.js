ab59ed7911251c5758cddb2b3fa531f7cd5ea3d2
root <root@server.diegomossmann.com>
2020-05-29 14:05:43 +0000

    ajuste server

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'mysql',
-  host: 'mysql669.umbler.com',
-  username: 'origem_userdb',
-  password: 'OrigVeic2233Bd',
-  database: 'origem_db',
+  host: 'localhost',
+  username: 'root',
+  password: '!@mq2233db',
+  database: 'origembd',
   define: {
     timestamps: true,
     underscored: true,
