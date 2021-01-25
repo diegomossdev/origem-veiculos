564fefc079518b0b105c95175b2f387ef2e4d38f
diegomossdev <60445532+diegomossdev@users.noreply.github.com>
2021-01-25 11:56:05 -0300

    Create database.js

---
+++ api/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'mysql669.umbler.com',
+  username: 'origem_userdb',
+  password: 'OrigVeic2233Bd',
+  database: 'origem_bd',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
